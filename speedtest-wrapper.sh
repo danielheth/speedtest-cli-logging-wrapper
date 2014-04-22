320f83cb14ead349b6819a9881cd60d03188a817
Pez Cuckow <email@pezcuckow.com>
2014-04-22 09:50:20 +0100

    Add output so it can be viewed in the logs

--- speedtest-wrapper.sh
+++ speedtest-wrapper.sh
@@ -9,8 +9,10 @@ waitTimeSeconds=3600
 
 if [ -r $timestampFile ] && [ $((`timestamp`-waitTimeSeconds)) -ge $(< $timestampFile) ] || [ ! -r $timestampFile ];
 then  
+  echo "Testing internet speed..."
   timestamp > $timestampFile
   (speedtest-cli | egrep -o '(.* \(.+\))|(\d+.\d+ Mbit/s)' | tr '\n' '\t'; echo) >> speedtest.log
+  echo "Done"
 fi
 
 
