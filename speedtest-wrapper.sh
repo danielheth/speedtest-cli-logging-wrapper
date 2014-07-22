49192fe77f7c80b6be81576a036e1ed44d9e3a16
danielheth <danielheth@hotmail.com>
2014-07-21 21:26:13 -0500

    changing log timestamp to human readable

--- speedtest-wrapper.sh
+++ speedtest-wrapper.sh
@@ -13,11 +13,12 @@ then
   echo "Testing internet speed..."
   
   TIMESTAMP=`date +"%s"`
+  DATETIME=`date +"%Y-%m-%d %H:%M:%S"`
   RESULTS=`speedtest-cli | grep 'Mbits/s' | awk -F':' '{print $2}' | awk -F' ' '{print $1;}' | awk 'NR%2{printf $0"|";next;}1'`
   DOWNLOAD=`echo $RESULTS | awk -F'|' '{print $1}'`
   UPLOAD=`echo $RESULTS | awk -F'|' '{print $2}'`
   
-  echo "$DOWNLOAD|$UPLOAD|$TIMESTAMP" >> /var/log/speedtest.log  
+  echo "$DOWNLOAD|$UPLOAD|$DATETIME" >> /var/log/speedtest.log  
   echo "$TIMESTAMP" > $timestampFile  
 
   echo "Done"
